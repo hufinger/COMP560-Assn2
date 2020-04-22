91f70410c6ce8d95b9363aa1d0e8a900ba93f490
Hunter Finger <36425349+hufinger@users.noreply.github.com>
2020-04-21 21:09:09 -0400

    All code for Reinforcement Learning assignment

---
+++ Action.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+public class Action {
+    private static final double DISCOUNT = 0.9;
+    private static final double ALPHA = 0.2;
+    private static final double REWARD = 1.0;
+    public static final double EXPLORATION_VALUE = 0.2;
+    private String name;
+    private double resultProbability;
+    private LinkedList<ActionState> actionStates;
+    private LinkedList<Integer> scores = new LinkedList<>();
+    private double count;
+    private ActionState resultActionState;
+    private double functionValue = 0.0;
+
+    public Action(String name){
+        this.name = name;
+        this.actionStates = new LinkedList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public LinkedList<ActionState> getActionStates() {
+        return this.actionStates;
+    }
+    public void addPair(State state, double prob, double count){
+        this.actionStates.add(new ActionState(state, prob, count));
+    }
+    public void printPairs(){
+        for (ActionState actionState : this.actionStates) {
+            State state = actionState.getState();
+            Double prob = actionState.getProbability();
+            Double count =  actionState.getCount();
+            System.out.println(state.getName() + " , Prob: " + prob + " , Count: " + count);
+        }
+    }
+
+    //Okay so this is a kinda screwy way of picking based on each resulting state probability
+    //So we have a map of (State, Probability) pairs and we have to shuffle them so that they don't get iterrated the same way everytime
+    //Then we randomly pick a probability from 0-1 (p) and set the cumulative total to 0.
+    //Then we iterate over each State and add each states probability to the totalprobabilty, once the total probability is greater than the random probability
+    //We return the most recent State that was added to totalProbability
+    //This was the best way I could think of, sorry for what is the overly complicated way of doing it but I'm no genius :)
+    public State chooseResultState(){
+        Random rand = new Random();
+        double p = Math.random();
+        Collections.shuffle(this.actionStates);
+        if(p<EXPLORATION_VALUE){
+            int x = rand.nextInt(this.actionStates.size());
+            State returnState = actionStates.get(x).getState();
+            this.resultProbability = actionStates.get(x).getProbability();
+            this.setResultActionState(this.actionStates.get(x));
+            return returnState;
+        }else{
+            double totalProbability = 0.0;
+
+            for (ActionState actionState : this.actionStates) {
+                totalProbability += actionState.getProbability();
+                if (p <= totalProbability) {
+                    State returnState = actionState.getState();
+                    this.resultProbability = actionState.getProbability();
+                    this.setResultActionState(actionState);
+                    return returnState;
+                }
+            }
+        }
+        return null;
+    }
+
+    public State chooseRandomResultState(){
+        Random rand = new Random();
+        int x = rand.nextInt(this.getActionStates().size());
+        return this.getActionStates().get(x).getState();
+    }
+
+    public void setResultActionState(ActionState actionState){
+        this.resultActionState = actionState;
+    }
+
+    public void updateResultActionState(){
+        this.resultActionState.addCount();
+    }
+
+
+    public void updateProbability(){
+        for (ActionState actionState : this.actionStates) {
+           actionState.updateProbability(this.count);
+        }
+    }
+    public double getResultProbability() {
+        return this.resultProbability;
+    }
+
+    public void updateCount(){
+        this.count++;
+    }
+
+
+    public double getFunctionValue() {
+        return this.functionValue;
+    }
+
+    public void setFunctionValue(double functionValue) {
+        this.functionValue = functionValue;
+    }
+
+    public void calculateFunctionValue(Action newAction){
+        functionValue = newAction.getFunctionValue() + ALPHA*(REWARD + DISCOUNT*this.functionValue - newAction.getFunctionValue());
+        this.setFunctionValue(functionValue);
+    }
+
+
+}
---
+++ Model.java
@@ -0,0 +1,176 @@
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Model {
+
+    private LinkedList<State> states;
+
+    public Model(LinkedList<State> states){
+        this.states = states;
+    }
+
+    public void learnModel(){
+        int count = 1;
+        double totalChosen = 0.0;
+        while(count <5000) {
+            System.out.println("ITERATION: " + count);
+//            printResults();
+            State state = this.getState("Fairway");
+            Action action = null;
+
+
+            while (!state.getName().equals("In")) {
+
+                String firstStateName = state.getName();
+                action = state.chooseAction();
+
+                State tempState = action.chooseResultState();
+                if(tempState == null) {
+
+                    while (tempState == null) {
+                        action = state.chooseAction();
+                        tempState = action.chooseResultState();
+
+                    }
+                }
+                action.updateCount();
+                action.updateResultActionState();
+                state = action.chooseResultState();
+
+                System.out.println(firstStateName+"/"+action.getName()+"/"+state.getName()+"/"+action.getResultProbability());
+
+            }
+            System.out.println("Hopefully we make it in the hole!: " + state.getName());
+            if(count%1000==0){
+                System.out.println("RECALCULATING");
+                updateAllProbability();
+            }
+            updateAllUtilities();
+            System.out.println();
+            count++;
+        }
+
+        System.out.println("////////////////////////////////");
+        printAllProbabilies();
+        System.out.println();
+        System.out.println("FINAL RESULTS: ");
+        printResults();
+    }
+
+
+    public void learnModelFree(){
+        int count = 1;
+        while(count <100) {
+            System.out.println("Episode: " + count);
+            int score = 1;
+            //Initialize S, Q(S,A) is already initialized to zero by creating the actions.
+            State state = this.getState("Fairway");
+            //choose A(S) using eplison greedy from Q
+            Action action = state.chooseActionGreedyQ();
+            while (!state.getName().equals("In")) {
+                //take action A, get reward, S'
+                State newState = action.chooseRandomResultState();
+                System.out.println(state.getName() + "/" + action.getName() + "/" + newState.getName() + " " + action.getFunctionValue());
+                System.out.println("");
+                if(newState.getName().equals("In")){break;}
+                //choose A'(S') using epsilon greedy from Q
+                Action newAction = newState.chooseActionGreedyQ();
+                //Q(s,a) -> Q(s,a) + alpha[Reward + gamma*Q(s',a') - Q(s,a)
+                action.calculateFunctionValue(newAction);
+                //A -> a'
+                action = newAction;
+                state = newState;
+                score++;
+            }
+            System.out.println("Hopefully we make it in the hole! with score: " + (score));
+            System.out.println();
+            count++;
+        }
+        setModelFreePolicies();
+//        printFunctionValues();
+    }
+
+    private void setModelFreePolicies(){
+        for(int i=  0; i<states.size(); i++){
+            states.get(i).setPolicyByFunctionValue();
+            System.out.println(states.get(i).getName() + ", Policy: " + states.get(i).getPolicy() + " " + states.get(i).getUtility());
+        }
+    }
+
+    private void printFunctionValues(){
+        for(int i = 0; i<states.size(); i++){
+            System.out.println("State: " + states.get(i).getName());
+            System.out.println("////////");
+            states.get(i).printFunctionValues();
+        }
+
+    }
+
+
+
+    private void updateAllProbability(){
+        for(int i = 0; i<states.size(); i++){
+            states.get(i).updateProbability();
+        }
+    }
+
+    private void updateAllUtilities(){
+        for(int i = 0; i<states.size(); i++){
+            states.get(i).updateUtility();
+        }
+    }
+
+    private void printUtilities(){
+        for(int i = 0; i<states.size(); i++){
+            System.out.println("State: " + states.get(i).getName() + ", Utility Value: " + states.get(i).getUtility() + ", Policy: " + states.get(i).getPolicy());
+        }
+    }
+
+    private void printAllProbabilies(){
+        for(int i = 0; i<states.size(); i++){
+            System.out.println(states.get(i).getName());
+            System.out.println("///////////");
+            for(int j = 0; j<states.get(i).getActions().size(); j++){
+                System.out.println(states.get(i).getActions().get(j).getName());
+                System.out.println("Action: " + states.get(i).getActions().get(j).getName() + " has these value pairs:");
+                states.get(i).getActions().get(j).printPairs();
+            }
+            System.out.println("///////////");
+
+        }
+    }
+
+
+    private void printResults(){
+        System.out.println("//////////////");
+        for(int i = 0; i<states.size(); i++){
+            states.get(i).updateUtility();
+            System.out.println("State: " + states.get(i).getName() + ", Utility: " + states.get(i).getUtility()  + ", Policy: " + states.get(i).getPolicy());
+        }
+        System.out.println("//////////////");
+    }
+
+
+    //helper function to get a specific state from the array of possible states in the model
+    private State getState(String state){
+        for(int i = 0; i<this.states.size(); i++){
+            if(this.states.get(i).getName().equals(state)){
+                return this.states.get(i);
+            }
+        }
+        //no state is contained in this model
+        return null;
+    }
+
+
+//    private void printPolicies(){
+//        System.out.println("//////////////");
+//        for(int i = 0; i<states.size(); i++){
+//            states.get(i).setPolicyByScore();
+//            System.out.println("State: " + states.get(i).getName() + ", Utility: " + ", Policy: " + states.get(i).getPolicy());
+//        }
+//    }
+
+
+
+}
